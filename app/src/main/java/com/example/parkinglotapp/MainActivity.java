d1e841828bc9f8e5475501a87753482a8d659c8f
tindijames <tin11006@byui.edu>
2020-02-18 22:55:36 -0800

    Merge branch 'master' of https://github.com/TLatu2020/ParkingLotApp
    # Conflicts:
    #	app/src/main/java/com/example/parkinglotapp/MainActivity.java

--- app/src/main/java/com/example/parkinglotapp/MainActivity.java
+++ app/src/main/java/com/example/parkinglotapp/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /*Tisi's Comment*/
         /*jake's comment*/
+        /*my turn to comment*/
     }
 }
 
