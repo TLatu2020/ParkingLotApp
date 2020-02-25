ee5a66330b9a43c228765f032a0ba7e39b9c2ad5
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2020-02-25 15:58:53 -0700

    Start of the Activities

--- app/src/main/java/com/example/parkinglotapp/ParkingLotPage.java
+++ app/src/main/java/com/example/parkinglotapp/ParkingLotPage.java
@@ -7,30 +7,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-
-public class MainActivity extends AppCompatActivity {
+public class ParkingLotPage extends AppCompatActivity {
 
     //This constant uses the name of the class itself as the tag
     private static final String LOG_TAG =
-            MainActivity.class.getSimpleName();
+            ParkingLotPage.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_parking_lot_page);
     }
 
     //Called when the user taps the View Parking Lot button
-    public void viewParkingLot(View view){
+    public void viewParkingSpot(View view){
 
         //Testing button
-        Log.d(LOG_TAG, "view parking lot button pushed!");
+        Log.d(LOG_TAG, "parking spot button pushed!");
 
-        Intent intent = new Intent (MainActivity.this, ParkingLotPage.class);
+        Intent intent = new Intent (ParkingLotPage.this, ParkingSpotPage.class);
         startActivity(intent);
-
     }
 }
-
-
-
